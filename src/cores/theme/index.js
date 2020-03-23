274c16bbd6430b9fb3b464ce7e285c3e3dd5569b
l3oss123456 <oncelemon02@gmail.com>
2020-03-23 22:37:05 +0700

    setting prettier

---
+++ src/cores/theme/index.js
@@ -0,0 +1,43 @@
+export const colors = {
+  white: "#ffffff",
+  black: "#000000",
+  gray: "#ffffff2b",
+  blackRussian: "#14162c",
+  primary: "#191c36",
+  midnightEx: "#1F2548",
+  midnightEx90: "#1E2242",
+  luckyPoint: "#2A2D5C",
+  deepKoamaru: "#33366E",
+  persianIndigo: "#3610a0",
+  parisM: "#34236D",
+  hanPurple: "#5018EF",
+  neonBlue: "#633dff",
+  royalBlue: "#2e5bff",
+  neonBlue90: "#4561ff",
+  alizarin: "#F5222D",
+  danger: "#ff0000",
+  ruby: "#e4007c",
+  magenta: "#ff00f4",
+  surfieGreen: "#007B75",
+  overviewAction: "#50a3a4",
+  brightTurquoise: "#00E4E4",
+  jade: "#00cc6d",
+  submit: "#2ede3c",
+  cinnabar: "#de502e",
+  lemon: "#EFDE18"
+};
+export const fontFamily = {
+  primary: "Proxima Nova Soft",
+  primaryBold: "Proxima Nova Soft Bold"
+};
+
+export const fontSize = {
+  title: "50px",
+  text: "16px"
+};
+
+export default {
+  colors,
+  fontFamily,
+  fontSize
+};
